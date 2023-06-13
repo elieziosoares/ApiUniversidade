9344c6e08fe4f161c8199d800c3de37344c29eab
Eliezio Soares <elieziosoares@gmail.com>
2023-06-13 13:30:20 -0300

    Add actions em DisciplinaController

--- Controllers/DisciplinaController.cs
+++ Controllers/DisciplinaController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using apiUniversidade.Context;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace apiUniversidade.Controllers;
 
@@ -21,14 +22,58 @@ public class DisciplinaController : ControllerBase
         _context = context;
     }
 
-    [HttpGet(Name = "disciplinas")]
-    public ActionResult<IEnumerable<Disciplina>> GetDisciplinas()
+    [HttpGet]
+    public ActionResult<IEnumerable<Disciplina>> Get()
     {
         var disciplinas = _context.Disciplinas.ToList();
         if(disciplinas is null)
             return NotFound();
 
         return disciplinas;
+    }
+
+    [HttpGet("{id:int}", Name="GetDisciplina")]
+    public ActionResult<Disciplina> Get(int id)
+    {
+        var disciplina = _context.Disciplinas.FirstOrDefault(p => p.Id == id);
+        if(disciplina is null)
+            return NotFound("Disciplina não encontrado.");
+
+        return disciplina;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Disciplina disciplina){
+        _context.Disciplinas.Add(disciplina);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("GetDisciplina",
+            new{ id = disciplina.Id},
+            disciplina);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Disciplina disciplina){
+        if(id != disciplina.Id)
+            return BadRequest();
+
+        _context.Entry(disciplina).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(disciplina);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id){
+        var disciplina = _context.Disciplinas.FirstOrDefault(p => p.Id == id);
+
+        if(disciplina is null)
+            return NotFound();
+
+        _context.Disciplinas.Remove(disciplina);
+        _context.SaveChanges();
+
+        return Ok(disciplina);
     }
 }
 
