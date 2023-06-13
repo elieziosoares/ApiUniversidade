2de687b21a95cdc51afa6bc4fb80d86e63442e57
Eliezio Soares <elieziosoares@gmail.com>
2023-06-13 11:24:34 -0300

    Inclusão das actions em CursoController

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using apiUniversidade.Context;
 using apiUniversidade.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace apiUniversidade.Controllers;
@@ -23,13 +24,57 @@ public class CursoController : Controller
         _context = context;
     }
 
-    [HttpGet(Name = "cursos")]
-    public ActionResult<IEnumerable<Curso>> GetCursos()
+    [HttpGet]
+    public ActionResult<IEnumerable<Curso>> Get()
     {
         var cursos = _context.Cursos.ToList();
         if(cursos is null)
             return NotFound();
 
         return cursos;
+    }
+
+    [HttpGet("{id:int}", Name="GetCurso")]
+    public ActionResult<Curso> Get(int id)
+    {
+        var curso = _context.Cursos.FirstOrDefault(p => p.Id == id);
+        if(curso is null)
+            return NotFound("Curso não encontrado.");
+
+        return curso;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Curso curso){
+        _context.Cursos.Add(curso);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("GetCurso",
+            new{ id = curso.Id},
+            curso);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Curso curso){
+        if(id != curso.Id)
+            return BadRequest();
+
+        _context.Entry(curso).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(curso);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id){
+        var curso = _context.Cursos.FirstOrDefault(p => p.Id == id);
+
+        if(curso is null)
+            return NotFound();
+
+        _context.Cursos.Remove(curso);
+        _context.SaveChanges();
+
+        return Ok(curso);
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
 builder.Services.AddDbContext<ApiUniversidadeContext>(options => options.UseNpgsql(connectionString));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,11 +16,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment()){
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
