494631649368f7f5b9440f34cab77ff604e85ccb
Eliezio Soares <elieziosoares@gmail.com>
2023-06-13 12:04:21 -0300

    ConnectionString alterada para apontar para Azure

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DbContext to access PostgreSQL
-var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
+var connectionString = builder.Configuration.GetConnectionString("Azure");
 builder.Services.AddDbContext<ApiUniversidadeContext>(options => options.UseNpgsql(connectionString));
 
 // Add services to the container.
